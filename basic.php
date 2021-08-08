d7116634021d38a0504ec1e21fdaee551c1b3efe
Uma Thileepan <thileepan_uma@yahoo.co.uk>
2021-08-08 12:19:51 +0100

    php basic practice

---
+++ basic.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- To run: In cmd: php -S localhost:4000 -->
+    <?php
+        echo ("Hello World");
+        echo "<h1> Add Two Numbers </h1>";
+        $num1 = 20;
+        $num2 = 30;
+        $sum = $num1 + $num2;
+        echo "<p> Total of two given numbers: $sum </p>";
+    ?>
+</body>
+</html>
